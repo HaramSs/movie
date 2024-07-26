ba216888197e9b1ad146b62c395b4bdf6a4cbc3d
HaramSs <haram8274@gmail.com>
2024-07-26 16:29:41 +0900

    테스트 추가

---
+++ tests/test_call.py
@@ -0,0 +1,30 @@
+from src.movie.api.call import gen_url, req, get_key, req2dataframe
+
+def test_비밀키숨기기():
+    key = get_key()
+    assert key
+
+def test_유알엘테스트():
+    url = gen_url()
+    assert "http" in url
+    assert "kobis" in url
+
+def test_req():
+    code, data = req()
+    assert code == 200
+
+    print(data)
+
+    code, data = req('20230101')
+    assert code == 200
+
+    print(data)
+
+def test_req2dataframe():
+    l = req2dataframe()
+    assert len(l) > 0
+    v = l[0]
+    assert 'rnum' in v.keys()
+    assert v['rnum'] == '1'
+    
+
